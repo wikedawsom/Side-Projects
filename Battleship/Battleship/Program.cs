d73291ea9fec41acd985ba0d943b699003cace06
Christian Harris <cp_harris1011@hotmail.com>
2019-09-24 11:34:33 -0400

    Added ship placement methods for manual and auto (random), added method to initialize boards so constructors are more compact

--- Battleship/Battleship/Program.cs
+++ Battleship/Battleship/Program.cs
@@ -11,8 +11,12 @@ namespace Battleship
 
             BattleshipBoard player1 = new BattleshipBoard();
             player1.ShowBoard();
-
-
+            
+            player1.PlaceShipsAuto();
+            Console.WriteLine("\n");
+                
+            player1.ShowBoard();
+            
             Console.Write("Program ended, press any key to exit window.");
             Console.ReadKey();
         }
@@ -24,31 +28,16 @@ namespace Battleship
         public int SideLength;
         private string[] ShipHealth;
         private int NumShipsAlive;
+        private Random numGen;
 
         public BattleshipBoard()
         {
+            numGen = new Random();
             SideLength = 10;
             ShipHealth = new string[] {"55555", "4444", "333","222","11"};
             NumShipsAlive = 5;
-            BoardSquares = new List<List<char>>();
-            HiddenBoardSquares = new List<List<char>>();
 
-            for (int row = 0; row < SideLength; row ++)
-            {
-                BoardSquares.Add(new List<char>());
-                for (int col = 0; col < SideLength; col++)
-                {
-                    BoardSquares[row].Add('O');
-                }
-            }
-            for (int row = 0; row < SideLength; row++)
-            {
-                HiddenBoardSquares.Add(new List<char>());
-                for (int col = 0; col < SideLength; col++)
-                {
-                    HiddenBoardSquares[row].Add('O');
-                }
-            }
+            InitBoards();
         }
 
         public BattleshipBoard(int userDefinedLength)
@@ -56,6 +45,15 @@ namespace Battleship
             SideLength = userDefinedLength;
             ShipHealth = new string[] { "55555", "4444", "333", "222", "11" };
             NumShipsAlive = 5;
+            InitBoards();
+        }
+
+        private void InitBoards()
+        {
+
+            BoardSquares = new List<List<char>>();
+            HiddenBoardSquares = new List<List<char>>();
+
             for (int row = 0; row < SideLength; row++)
             {
                 BoardSquares.Add(new List<char>());
@@ -74,13 +72,63 @@ namespace Battleship
             }
         }
 
-        public void PlaceShipsManual()
+        public bool PlaceShipManual(int row, int col, int shipNum, char direction)
         {
+            bool placementSuccessful = false;
+            int curRow = 0;
+            int curCol = 0;
 
+            if (CheckValidShipPlacement(row, col, shipNum, direction))
+            {
+                for (int i = 0; i < ShipHealth[shipNum].Length; i++)
+                {
+                    if (direction == 'u')
+                    {
+                        curRow = row - i;
+                        curCol = col;
+                    }
+                    if (direction == 'd')
+                    {
+                        curRow = row + i;
+                        curCol = col;
+                    }
+                    if (direction == 'l')
+                    {
+                        curRow = row;
+                        curCol = col - i;
+                    }
+                    if (direction == 'r')
+                    {
+                        curRow = row;
+                        curCol = col + i;
+                    }
+                    BoardSquares[curRow][curCol] = ShipHealth[shipNum][i];
+                    placementSuccessful = true;
+                }
+            }
+
+            return placementSuccessful;
         }
 
         public void PlaceShipsAuto()
         {
+            int row = 0;
+            int col = 0;
+            int shipNum;
+            char[] possibleDirections = { 'u', 'd', 'l', 'r' };
+            char direction;
+
+            for (int i = 0; i < ShipHealth.Length; i++)
+            {
+                direction = possibleDirections[numGen.Next(0, 4)];
+                row = numGen.Next(0, SideLength);
+                col = numGen.Next(0, SideLength);
+                shipNum = i;
+                if(!PlaceShipManual(row, col, shipNum, direction))
+                {
+                    i--;
+                }
+            }
 
         }
 
@@ -109,13 +157,13 @@ namespace Battleship
             bool isValid = true;
             for (int i = 0; i < ShipHealth[shipNum].Length; i++)
             {
-                if (direction == 'u' && row - i >= 0 && BoardSquares[row - i][col] != 'O')
+                if (direction == 'u' && (row - i < 0 || BoardSquares[row - i][col] != 'O'))
                     isValid = false;
-                if (direction == 'd' && row + i < SideLength && BoardSquares[row + i][col] != 'O')
+                else if (direction == 'd' && (row + i >= SideLength || BoardSquares[row + i][col] != 'O'))
                     isValid = false;
-                if (direction == 'l' && col - i >= 0 && BoardSquares[row][col - i] != 'O')
+                else if (direction == 'l' && (col - i < 0 || BoardSquares[row][col - i] != 'O'))
                     isValid = false;
-                if (direction == 'r' && col + i < SideLength && BoardSquares[row][col + i] != 'O')
+                else if (direction == 'r' && (col + i >= SideLength || BoardSquares[row][col + i] != 'O'))
                     isValid = false;
             }
             return isValid;
