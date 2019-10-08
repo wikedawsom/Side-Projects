f3c81f103252843c236e429e54592440005956a1
Christian Harris <cp_harris1011@hotmail.com>
2019-10-08 14:13:47 -0400

    only correctly works for 1 frame per second... need to refactor maths

---
+++ TestingGround/FramerateStabilizer/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace FramerateStabilizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            BetterStableFrames consistentFramerateTracker = new BetterStableFrames();
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+
+            for (int i = 0; i < 5; i++)
+            {
+                StableFrames.Stabilize(1, timer);
+                //consistentFramerateTracker.Stabilize(60, timer);
+            }
+            Console.WriteLine(timer.ElapsedMilliseconds);
+            timer.Stop();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TestingGround/FramerateStabilizer/StableFrames.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading;
+
+namespace FramerateStabilizer
+{
+    public static class StableFrames
+    {
+        
+        public static void Stabilize(int iterationsPerSecond, Stopwatch timer)
+        {
+            double secondsPerIteraion = 1.0 / iterationsPerSecond;
+            double milisecondsPerIteration = secondsPerIteraion * 1000;
+            double sleepTime = milisecondsPerIteration - (timer.ElapsedMilliseconds % milisecondsPerIteration);
+            Thread.Sleep((int)sleepTime);
+        }
+    }
+
+    public class BetterStableFrames
+    {
+        private static long _currentFrame;
+        public BetterStableFrames()
+        {
+            _currentFrame = 0;
+        }
+        public void Stabilize(int iterationsPerSecond, Stopwatch timer)
+        {
+            if (iterationsPerSecond == 0)
+            {
+                return;
+            }
+            double secondsPerIteraion = 1.0 / iterationsPerSecond;
+            double milisecondsPerIteration = secondsPerIteraion * 1000;
+            double sleepTime = 0;
+            long currentFrame = timer.ElapsedMilliseconds / (long)milisecondsPerIteration;
+            if (currentFrame == _currentFrame)
+            {
+                sleepTime = milisecondsPerIteration - (timer.ElapsedMilliseconds % milisecondsPerIteration);
+                _currentFrame++;
+            }
+            else
+            {
+                _currentFrame = currentFrame;
+            }
+            Thread.Sleep((int)sleepTime);
+        }
+    }
+}
