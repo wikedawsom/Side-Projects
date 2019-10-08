ef08326a9b9443541f0819f540c91eea278ce99f
Christian Harris <cp_harris1011@hotmail.com>
2019-10-08 14:11:49 -0400

    created basic and recursive file extension changer

---
+++ TestingGround/FileExtentionRenamer/EasyFileNameChanger.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace FileExtentionRenamer
+{
+    public class FileManip
+    {
+        public void FileExtentionRename(string currentFilePath, string newFileExtention)
+        {
+            //string [] directories = Directory.GetDirectories(currentFilePath.Substring(0,currentFilePath.LastIndexOf('\\')));
+            File.Copy(currentFilePath, currentFilePath.Substring(0, currentFilePath.LastIndexOf("."))+newFileExtention);
+
+        }
+
+        public void RecursiveExtentionRename(string directory, string currentExtention, string newExtention)
+        {
+            string[] directories = Directory.GetDirectories(directory);
+            string[] files = Directory.GetFiles(directory);
+            foreach (string subDirectory in directories)
+            {
+                RecursiveExtentionRename(subDirectory, currentExtention, newExtention);
+            }
+            foreach(string file in files)
+            {
+                if (file.Substring(file.LastIndexOf("."),currentExtention.Length) == currentExtention)
+                {
+                    File.Copy(file, file.Substring(0, file.Length - currentExtention.Length) + newExtention);
+                }
+            }
+               
+        }
+        
+    }
+}
