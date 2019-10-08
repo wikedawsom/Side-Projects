68445de9fcb208774988da29ec7038bd6a690a56
Christian Harris <cp_harris1011@hotmail.com>
2019-10-08 14:20:14 -0400

    Data manipulation works well for text files, might add something for binary files later

--- TextFileEncoder/TextFileEncoder/Encoder.cs
+++ TextFileEncoder/TextFileEncoder/Encoder.cs
@@ -5,28 +5,83 @@ namespace TextFileEncoder
 {
     public static class FileEncoder
     {
-        public static void Main(string[] args)
-        { }
-        public static void EncodeFileStandard(string path)
+        /// <summary>
+        /// Reads a file as an array of bytes, then adds the offset to each byte
+        /// </summary>
+        /// <param name="path">The fully qualified file name</param>
+        /// <param name="offset">Optional byte value offset (default 1)</param>
+        public static void EncryptFileStandard(string path, byte offset = 1)
         {
             byte[] fileContents = File.ReadAllBytes(path);
 
             for (int i = 0; i < fileContents.Length; i++)
             {
-                fileContents[i]++;
+                fileContents[i] += offset;
             }
-            File.WriteAllBytes(path.Substring(0,path.LastIndexOf(".")) + ".encoded", fileContents);
+            File.WriteAllBytes(path.Substring(0,path.LastIndexOf(".")) + ".enc", fileContents);
         }
 
-        public static void DecodeFileStandard(string path)
+        /// <summary>
+        /// Reads a file that was encoded using the EncodeFileStandard() method
+        /// as an array of bytes, then subtracts the specified offset from each byte
+        /// </summary>
+        /// <param name="path">The fully qualified file name</param>
+        /// <param name="offset">Optional byte value offset (default 1)</param>
+        public static void DecryptFileStandard(string path, byte offset = 1)
         {
             byte[] fileContents = File.ReadAllBytes(path);
 
             for (int i = 0; i < fileContents.Length; i++)
             {
-                fileContents[i]--;
+                fileContents[i] -= offset;
             }
-            File.WriteAllBytes(path.Substring(0, path.LastIndexOf(".") + 1) + "-de.txt", fileContents);
+            File.WriteAllBytes(path.Substring(0, path.LastIndexOf(".")) + "-dec.txt", fileContents);
+        }
+
+        /// <summary>
+        /// Same principle as EncryptFileStandard(), except only reading the file
+        /// one character at a time to avoid problems caused by too much RAM usage
+        /// </summary>
+        /// <param name="path">The fully qualified file name</param>
+        /// <param name="offset">Specified value offset</param>
+        public static void EncryptFileStream(string path, int offset = 1)
+        {
+            StreamWriter sw = new StreamWriter(path.Substring(0, path.LastIndexOf(".")) + ".enc");
+            
+            using (StreamReader sr = new StreamReader(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    int character = sr.Read();
+                    character += offset;
+                    sw.Write((char)character);
+                }
+            }
+            sw.Close();
+        }
+
+        /// <summary>
+        /// Same principle as DecryptFileStandard(), except only reading the file
+        /// one character at a time to avoid problems caused by too much RAM usage
+        /// </summary>
+        /// <param name="path">The fully qualified file name</param>
+        /// <param name="offset">Specified value offset</param>
+        public static void DecryptFileStream(string path, int offset = 1)
+        {
+            StreamWriter sw = new StreamWriter(path.Substring(0, path.LastIndexOf(".")) + "-dec.txt");
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    int character = sr.Read();
+                    character -= offset;
+                    sw.Write((char)character);
+                }
+            }
+            sw.Close();
         }
     }
+
+
 }
--- TextFileEncoder/TextFileEncoderCLI/Program.cs
+++ TextFileEncoder/TextFileEncoderCLI/Program.cs
@@ -14,16 +14,42 @@ namespace TextFileEncoderCLI
             path += Console.ReadLine();
 
             Console.Write("Encode or decode? (e/d): ");
-            char userInput = Console.ReadKey().KeyChar;
-            if (userInput == 'e')
-            {
-                FileEncoder.EncodeFileStandard(path);
-            }
-            else if (userInput == 'd')
-            {
-                FileEncoder.DecodeFileStandard(path);
-            }
+            char userInputED = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+            Console.Write("Small or large file? (s/l): ");
+            char userInputSL = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+            Console.Write("Specify offset count: ");
 
+            int offset = 1;
+            int.TryParse(Console.ReadLine(), out offset);
+
+            Console.WriteLine("Working...");
+            try
+            {
+                if (userInputED == 'e' && userInputSL == 's')
+                {
+                    FileEncoder.EncryptFileStandard(path, (byte)offset);
+                }
+                else if (userInputED == 'd' && userInputSL == 's')
+                {
+                    FileEncoder.DecryptFileStandard(path, (byte)offset);
+                }
+                else if (userInputED == 'e' && userInputSL == 'l')
+                {
+                    FileEncoder.EncryptFileStream(path, (byte)offset);
+                }
+                else if (userInputED == 'd' && userInputSL == 'l')
+                {
+                    FileEncoder.DecryptFileStream(path, (byte)offset);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("File read/write failed. Please ensure you have a correct path and appropriate permissions.");
+                Console.ReadKey();
+            }
+            Console.WriteLine("Program Finished");
             Console.ReadKey();
         }
     }
