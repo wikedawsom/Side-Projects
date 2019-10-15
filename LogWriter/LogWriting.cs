35203639f7b4020c3ecd00ff7e4abca970cf0ba1
wikedawsom <cp_harris1011@hotmail.com>
2019-10-15 00:34:19 -0400

    Removed plaintext OAuth token from file. Bot reading chat, responding, and writing log files correctly

---
+++ LogWriter/LogWriting.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace LogWriting
+{
+    public class LogWriter
+    {
+        private string _fileName;
+        private StreamWriter _sw;
+
+        private DateTime Timestamp
+        {
+            get
+            {
+                return DateTime.Now;
+            }
+        }
+
+        public LogWriter(string filePath)
+        {
+            if (filePath.Contains("\\") && !Directory.Exists(filePath.Substring(0, filePath.LastIndexOf("\\"))))
+            {
+                Directory.CreateDirectory(filePath.Substring(0, filePath.LastIndexOf("\\")));
+            }
+            _fileName = filePath + ".log";
+        }
+        
+        public void NewLogEntry(string message)
+        {
+            using (_sw = new StreamWriter(_fileName, true))
+            {
+                _sw.WriteLine(Timestamp + " -- " + message);
+            }
+        }
+    }
+}
---
+++ TwitchChatBot/WikeBot/BotMaster.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using System.Net.Sockets;
+using System.Threading;
+using LogWriting;
+
+namespace WikeBot
+{
+    public static class BotMaster
+    {
+        private static string _userName = "wikedabot";
+        private static string _streamName = "wike";
+        private static string _oAuth = "";
+        private static LogWriter _messageLog = new LogWriter(@"logs\chat-messages");
+        private static LogWriter _eventLog = new LogWriter(@"logs\irc-messages");
+
+        public static void MainLoop()
+        {
+            GetOAuth();
+            var client = new IRCClient("irc.twitch.tv", 6667, _userName, _oAuth, _streamName);
+            
+            bool exit = false;
+            while (!exit)
+            {
+                string ircMessage = client.ReadMessage();
+
+                _eventLog.NewLogEntry(ircMessage);
+
+                if (ircMessage.Contains("PRIVMSG"))
+                {
+                    string messengerName = ircMessage[1..ircMessage.IndexOf("!")];
+                    string onlyMessage = ircMessage.Substring(ircMessage.IndexOf(":", 1) + 1);
+                    string fullUserMessage = $"{messengerName} :{onlyMessage}";
+                    
+                    _messageLog.NewLogEntry(fullUserMessage);
+
+                    if (onlyMessage == "!exit" && ircMessage.Substring(0, 20).Contains("wike"))
+                    {
+                        RespondInChat(client, "wike says its time for me to go.., bye all");
+                        exit = true;
+                    }
+                    else if (onlyMessage == "!help")
+                    {
+                        RespondInChat(client, $"{messengerName}... I cannot help you. No one can help you.");
+                    }
+                }
+            }
+        }
+
+        private static void RespondInChat(IRCClient client, string response)
+        {
+            client.SendChatMessage(response);
+            _messageLog.NewLogEntry("wikedabot :" + response);
+            _eventLog.NewLogEntry("wikedabot :" + response);
+        }
+
+        private static void GetOAuth()
+        {
+            _oAuth = ""; // Add code here to read OAuth from a file
+        }
+    }
+}
