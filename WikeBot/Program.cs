c5ebb51ad196f9592986d7ea031ee877bcd677d7
wikedawsom <cp_harris1011@hotmail.com>
2019-10-14 20:58:06 -0400

    started making an IRC chat bot for when I get a chance to livestream on Twitch again

---
+++ WikeBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WikeBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Party time");
+        }
+    }
+}
